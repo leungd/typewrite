624b3c9ddc34dada5cee8d1b7a48ca6870da5774
Derek Leung <me@derekleung.ca>
2014-09-23 21:55:05 -0400

    create layout.slim and setup initial style assets

--- app.rb
+++ app.rb
@@ -1,24 +1,26 @@
 class App < Sinatra::Base
+	
   register Sinatra::AssetPack
+
   assets do
 	serve '/js', from: 'js'
 	serve '/bower_components', from: 'bower_components'
 
 	js :libs, [
-	'/bower_components/jquery/dist/jquery.js',
-	'/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap-sprockets.js',
-	'/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js'
+		'/bower_components/jquery/dist/jquery.js',
+		'/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap-sprockets.js',
+		'/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js'
 	]
 
 	js :application, [
-	'/js/app.js'
+		'/js/app.js'
 	]
 
 	js_compression :jsmin
   end
   
   get '/' do
-  	slim :default
+  	slim :index
   end
 
 end
